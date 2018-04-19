4375d9b348df718b041bc972118b8eba31a2ed42
VintageVoodoo13 <32887419+VintageVoodoo13@users.noreply.github.com>
2018-04-19 16:54:29 -0500

    Add files via upload

---
+++ javascript/firebase.js
@@ -0,0 +1,15 @@
+  // Initialize Firebase
+
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCdXxbSPV8yNV4fsZ6LigHz8_hzPOISfHg",
+    authDomain: "rps-multiplayer-612f6.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-612f6.firebaseio.com",
+    projectId: "rps-multiplayer-612f6",
+    storageBucket: "rps-multiplayer-612f6.appspot.com",
+    messagingSenderId: "695734867690"
+  };
+  firebase.initializeApp(config);
+</script>
